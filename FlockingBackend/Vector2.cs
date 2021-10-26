7cbf5da6d426589085e4d626c2b62355fb81a7e5
Rod Rivas <rodrigo.rivasalfaro@dawsoncollege.qc.ca>
2021-10-26 16:41:53 -0400

    add Vector2

---
+++ FlockingBackend/Vector2.cs
@@ -0,0 +1,17 @@
+using System;
+
+namespace FlockingBackend
+{
+    public class Vector2
+    {
+        private float vx;
+        private float vy;
+
+        public float Vx{
+            get{ return vx;}
+        }
+        public float Vy{
+            get{ return vy;}
+        }
+    }
+}
