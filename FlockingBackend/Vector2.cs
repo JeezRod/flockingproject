9b91ca4a96fd4df3159112c491e58e1ae1f9353c
Rod Rivas <rodrigo.rivasalfaro@dawsoncollege.qc.ca>
2021-10-26 18:38:41 -0400

    add Overload operators

--- FlockingBackend/Vector2.cs
+++ FlockingBackend/Vector2.cs
@@ -2,16 +2,33 @@ using System;
 
 namespace FlockingBackend
 {
-    public class Vector2
+    public struct Vector2
     {
         private float vx;
         private float vy;
+
+        public Vector2(float vx, float vy){
+            this.vx = vx;
+            this.vy = vy;
+        }
 
         public float Vx{
             get{ return vx;}
         }
         public float Vy{
             get{ return vy;}
+        }
+        /*
+        *   Overloading the sum operator to sum vectors.
+        */
+        public static Vector2 operator +(Vector2 u, Vector2 v){
+            return new Vector2(u.vx + v.vx, u.vy + v.vy);
+        }
+        /*
+        *   Overloading the minus operator to substract vectors.
+        */
+        public static Vector2 operator -(Vector2 u, Vector2 v){
+            return new Vector2(u.vx - v.vx, u.vy - v.vy);
         }
     }
 }
