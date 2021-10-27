88456e06b4c054b6094d0342a82ff89b318a5763
Rod Rivas <rodrigo.rivasalfaro@dawsoncollege.qc.ca>
2021-10-27 00:00:13 -0400

    add overloaded operators

--- FlockingBackend/Vector2.cs
+++ FlockingBackend/Vector2.cs
@@ -18,17 +18,40 @@ namespace FlockingBackend
         public float Vy{
             get{ return vy;}
         }
+
         /*
         *   Overloading the sum operator to sum vectors.
         */
         public static Vector2 operator +(Vector2 u, Vector2 v){
             return new Vector2(u.vx + v.vx, u.vy + v.vy);
         }
+
         /*
         *   Overloading the minus operator to substract vectors.
         */
         public static Vector2 operator -(Vector2 u, Vector2 v){
             return new Vector2(u.vx - v.vx, u.vy - v.vy);
         }
+
+        /*
+        *   Overloading the dividend operator to divide vectors by a scalar.
+        */
+        public static Vector2 operator /(Vector2 u, float c){
+            return new Vector2(u.vx/c, u.vy/c);
+        }
+
+        /*
+        *   Overloading the product operator to multiply a vector and scalar.
+        */
+
+        public static Vector2 operator *(Vector2 u, float c){
+            return new Vector2(u.vx * c, u.vy * c);
+        }
+
+        public override string ToString()
+        {
+            return vx +", "+ vy;
+        }
+
     }
 }
