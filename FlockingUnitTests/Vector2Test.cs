0765603f4834d27310b68bc33fdaa4e52a74ef1c
Rod Rivas <rodrigo.rivasalfaro@dawsoncollege.qc.ca>
2021-10-29 12:46:36 -0400

    add test for 2 methods

--- FlockingUnitTests/Vector2Test.cs
+++ FlockingUnitTests/Vector2Test.cs
@@ -1,5 +1,6 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using FlockingBackend;
+using System;
 
 namespace FlockingUnitTests
 {
@@ -45,5 +46,25 @@ namespace FlockingUnitTests
 
             Assert.AreEqual(result, v1*c);
         }
+
+        [TestMethod]
+        public void Vector2DistanceSquaredTest()
+        {
+            float result = 2236068.0E-6f;
+            Vector2 v1 = new Vector2(200.0E-2f, 300.0E-2f);
+            Vector2 v2 = new Vector2(100.0E-2f, 100.0E-2f);
+
+            Assert.AreEqual(result, Vector2.DistanceSquared(v1,v2));
+        }
+
+        [TestMethod]
+        public void Vector2NormalizeVectorTest()
+        {
+            Vector2 result = new Vector2(0554700196.0E-9f, 0832050300.0E-9f);
+            Vector2 v1 = new Vector2(200.0E-2f, 300.0E-2f);
+            
+            Assert.AreEqual(result, Vector2.NormalizeVector(v1));
+        }
+
     }
 }
